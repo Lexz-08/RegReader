af9a56832733ac2d71666932e4b0409f12e0fcd2
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2022-08-18 20:38:28 -0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace RegReader
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.lblPath = new CometUI.CometLabel();
+			this.txtPath = new CometUI.CometTextBox();
+			this.txtKey = new CometUI.CometTextBox();
+			this.lblKey = new CometUI.CometLabel();
+			this.btnReadKey = new CometUI.CometFlatButton();
+			this.txtReadResult = new CometUI.CometMultilineTextBox();
+			this.txtValue = new CometUI.CometTextBox();
+			this.lblValue = new CometUI.CometLabel();
+			this.SuspendLayout();
+			// 
+			// lblPath
+			// 
+			this.lblPath.AutoSize = true;
+			this.lblPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.lblPath.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.lblPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.lblPath.Location = new System.Drawing.Point(12, 51);
+			this.lblPath.Name = "lblPath";
+			this.lblPath.Size = new System.Drawing.Size(40, 19);
+			this.lblPath.TabIndex = 0;
+			this.lblPath.Text = "Path:";
+			// 
+			// txtPath
+			// 
+			this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.txtPath.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtPath.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtPath.FocusedBorderColor = System.Drawing.Color.DeepSkyBlue;
+			this.txtPath.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.txtPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtPath.Location = new System.Drawing.Point(63, 48);
+			this.txtPath.Multiline = false;
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Readonly = false;
+			this.txtPath.Size = new System.Drawing.Size(418, 26);
+			this.txtPath.TabIndex = 1;
+			this.txtPath.UnderlineBorder = false;
+			this.txtPath.UseSystemPasswordChar = false;
+			// 
+			// txtKey
+			// 
+			this.txtKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtKey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.txtKey.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtKey.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtKey.FocusedBorderColor = System.Drawing.Color.DeepSkyBlue;
+			this.txtKey.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.txtKey.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtKey.Location = new System.Drawing.Point(63, 80);
+			this.txtKey.Multiline = false;
+			this.txtKey.Name = "txtKey";
+			this.txtKey.Readonly = false;
+			this.txtKey.Size = new System.Drawing.Size(418, 26);
+			this.txtKey.TabIndex = 3;
+			this.txtKey.UnderlineBorder = false;
+			this.txtKey.UseSystemPasswordChar = false;
+			// 
+			// lblKey
+			// 
+			this.lblKey.AutoSize = true;
+			this.lblKey.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.lblKey.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.lblKey.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.lblKey.Location = new System.Drawing.Point(12, 83);
+			this.lblKey.Name = "lblKey";
+			this.lblKey.Size = new System.Drawing.Size(34, 19);
+			this.lblKey.TabIndex = 2;
+			this.lblKey.Text = "Key:";
+			// 
+			// btnReadKey
+			// 
+			this.btnReadKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnReadKey.BorderWidth = 1;
+			this.btnReadKey.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnReadKey.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.btnReadKey.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.btnReadKey.Location = new System.Drawing.Point(407, 144);
+			this.btnReadKey.Name = "btnReadKey";
+			this.btnReadKey.Size = new System.Drawing.Size(74, 28);
+			this.btnReadKey.TabIndex = 4;
+			this.btnReadKey.Text = "Read Key";
+			this.btnReadKey.Click += new System.EventHandler(this.ReadSubkey);
+			// 
+			// txtReadResult
+			// 
+			this.txtReadResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtReadResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.txtReadResult.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtReadResult.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtReadResult.FocusedBorderColor = System.Drawing.Color.DeepSkyBlue;
+			this.txtReadResult.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.txtReadResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtReadResult.Location = new System.Drawing.Point(63, 144);
+			this.txtReadResult.Name = "txtReadResult";
+			this.txtReadResult.Readonly = false;
+			this.txtReadResult.Size = new System.Drawing.Size(338, 139);
+			this.txtReadResult.TabIndex = 5;
+			this.txtReadResult.UnderlineBorder = false;
+			// 
+			// txtValue
+			// 
+			this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.txtValue.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtValue.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.txtValue.FocusedBorderColor = System.Drawing.Color.DeepSkyBlue;
+			this.txtValue.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.txtValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.txtValue.Location = new System.Drawing.Point(63, 112);
+			this.txtValue.Multiline = false;
+			this.txtValue.Name = "txtValue";
+			this.txtValue.Readonly = false;
+			this.txtValue.Size = new System.Drawing.Size(418, 26);
+			this.txtValue.TabIndex = 7;
+			this.txtValue.UnderlineBorder = false;
+			this.txtValue.UseSystemPasswordChar = false;
+			// 
+			// lblValue
+			// 
+			this.lblValue.AutoSize = true;
+			this.lblValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+			this.lblValue.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.lblValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+			this.lblValue.Location = new System.Drawing.Point(12, 115);
+			this.lblValue.Name = "lblValue";
+			this.lblValue.Size = new System.Drawing.Size(45, 19);
+			this.lblValue.TabIndex = 6;
+			this.lblValue.Text = "Value:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(493, 295);
+			this.Controls.Add(this.txtValue);
+			this.Controls.Add(this.lblValue);
+			this.Controls.Add(this.txtReadResult);
+			this.Controls.Add(this.btnReadKey);
+			this.Controls.Add(this.txtKey);
+			this.Controls.Add(this.lblKey);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.lblPath);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeColor = System.Drawing.Color.DeepSkyBlue;
+			this.MinimizeColor = System.Drawing.Color.DeepSkyBlue;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "RegReader";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private CometUI.CometLabel lblPath;
+		private CometUI.CometTextBox txtPath;
+		private CometUI.CometTextBox txtKey;
+		private CometUI.CometLabel lblKey;
+		private CometUI.CometFlatButton btnReadKey;
+		private CometUI.CometMultilineTextBox txtReadResult;
+		private CometUI.CometTextBox txtValue;
+		private CometUI.CometLabel lblValue;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,99 @@
+﻿using CometUI;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegReader
+{
+	public partial class MainForm : CometForm
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		#region Registry Reader
+
+		private Dictionary<string, RegistryKey> _keyHives = new Dictionary<string, RegistryKey>
+		{
+			{ "HKEY_CLASSES_ROOT", Registry.ClassesRoot },
+			{ "HKEY_CURRENT_USER", Registry.CurrentUser },
+			{ "HKEY_LOCAL_MACHINE", Registry.LocalMachine },
+			{ "HKEY_USERS", Registry.Users },
+			{ "HKEY_CURRENT_CONFIG", Registry.CurrentConfig },
+		};
+
+		private void ReadSubkey(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(txtPath.Text))
+			{
+				MessageBox.Show("Unable to read Registry because no path to a key was specified.", "Key Path Required",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			else if (string.IsNullOrEmpty(txtKey.Text))
+			{
+				MessageBox.Show("Unable to read Registry because no key to read was specified.", "Key Name Required",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			else if (string.IsNullOrEmpty(txtValue.Text))
+			{
+				MessageBox.Show("Unable to read Registry because no value to read was specified.", "Key Value Name Required",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			try
+			{
+				string keyHiveName = txtPath.Text.Substring(0, txtPath.Text.IndexOf('\\'));
+				RegistryKey selectedHive = _keyHives[keyHiveName];
+				object keyValue = selectedHive.OpenSubKey(txtPath.Text.Substring(keyHiveName.Length + 1) + "\\" + txtKey.Text).GetValue(txtValue.Text);
+				Type valueType = keyValue.GetType();
+
+				if (valueType == typeof(string))
+					txtReadResult.Text = keyValue.ToString();
+				else if (valueType == typeof(byte[]))
+					txtReadResult.Text = ReadBinaryValue((byte[])keyValue);
+				else if (valueType == typeof(int))
+					txtReadResult.Text = ReadDWORDValue((int)keyValue);
+				else if (valueType == typeof(long))
+					txtReadResult.Text = ReadQWORDValue((long)keyValue);
+			}
+			catch (Exception exc)
+			{
+				MessageBox.Show("An error has occurred when attempting to read the specified key/value.\n" +
+					"This is because either the root key specified is not supported, the key specified does not exist, or the value specified does not exist.\n\n" +
+					"Please make sure you have entered the information correctly when attempting to read the Registry.", "Unable To Read Registry",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show("For debug purposes, the love of errors, or just if you are testing this program's capabilities, here is the error information:\n\n" +
+					exc.Message + "\n\nStack Trace:\n" + exc.StackTrace, "Error Debug Info",
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private string ReadBinaryValue(byte[] value)
+		{
+			StringBuilder strBuild = new StringBuilder();
+
+			foreach (byte binary in value)
+				strBuild.Append((char)binary);
+
+			return strBuild.ToString();
+		}
+
+		private string ReadDWORDValue(int value)
+		{
+			return value.ToString();
+		}
+
+		private string ReadQWORDValue(long value)
+		{
+			return value.ToString() + 'L';
+		}
+
+		#endregion
+	}
+}
